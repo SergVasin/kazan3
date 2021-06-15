7bce71b8f8e2a1a73661f9cf7359e1aee8deec3e
SergVasin <66994038+SergVasin@users.noreply.github.com>
2021-06-15 08:41:47 +0400

    Update script.js

--- script.js
+++ script.js
@@ -849,5 +849,14 @@ $( document ).ready(function() {
 	});
 });
 
+$("img").each(function() {
+	var $this = $(this);
+	var src = $this.attr("src");
+	$this.attr( "data-src", src );
+	$this.removeAttr("src");
+}); 
+
+$('img').addClass("lazyload");
+
 
 
